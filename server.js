ac8246e7654cfc9d6a26d40e47314c4c18c81608
rambharlia007 <rambharlia007@gmail.com>
2019-05-26 16:57:13 +0530

    heroku commit issur

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -34,13 +34,13 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+if (true || process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 const port = process.env.PORT || 5000;
